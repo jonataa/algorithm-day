3ddf758b735cb49338e0d9ddc11add3915c0c222
Jonata Weber <jonataa@gmail.com>
2016-06-22 13:47:40 -0300

    Fazer o teste ficar verde

--- src/jokenpo.js
+++ src/jokenpo.js
@@ -1,20 +1,39 @@
-const labels = {
-  EMPATE:  'empate',
-  PEDRA:   'pedra',
-  PAPEL:   'papel',
-  TESOURA: 'tesoura'
-}; 
+const EMPATE  = 'empate',
+      PEDRA   = 'pedra',
+      PAPEL   = 'papel',
+      TESOURA = 'tesoura';
+
+var jogadas = {};
+
+jogadas[PEDRA] = {};
+jogadas[PEDRA][TESOURA] = PEDRA;
+jogadas[PEDRA][PAPEL] = PAPEL;
+jogadas[TESOURA] = {};
+jogadas[TESOURA][PEDRA] = PEDRA;
+jogadas[TESOURA][PAPEL] = TESOURA;
+jogadas[PAPEL] = {};
+jogadas[PAPEL][PEDRA] = PAPEL;
+jogadas[PAPEL][TESOURA] = TESOURA;
 
 var jokenpo = {
   
-  labels: labels,
+  EMPATE: EMPATE,
+  PEDRA: PEDRA,
+  PAPEL: PAPEL,
+  TESOURA: TESOURA,
 
   jogar: function (x, y) {
     if (x === y) {
-      return labels.EMPATE;
+      return EMPATE;
     }
+
+    if (typeof jogadas[x] !== 'undefined'
+        && typeof jogadas[x][y] !== 'undefined') {
+      return jogadas[x][y];
+    }
+
   }
-  
+
 };
 
 module.exports = jokenpo;
--- test/test_jokenpo.js
+++ test/test_jokenpo.js
@@ -1,23 +1,22 @@
 var assert  = require('chai').assert,
-    jokenpo = require('../src/jokenpo.js'),
-    labels  = jokenpo.labels;
+    jokenpo = require('../src/jokenpo.js');
 
 describe('Jokenpo', function() {
   describe('pedra empata com pedra e ganha de tesoura', function () {
-    assert.equal(labels.EMPATE, jokenpo.jogar(labels.PEDRA, labels.PEDRA));
-    assert.equal(labels.PEDRA, jokenpo.jogar(labels.PEDRA, labels.TESOURA));
-    assert.equal(labels.PEDRA, jokenpo.jogar(labels.TESOURA, labels.PEDRA));
+    assert.equal(jokenpo.EMPATE, jokenpo.jogar(jokenpo.PEDRA, jokenpo.PEDRA));
+    assert.equal(jokenpo.PEDRA, jokenpo.jogar(jokenpo.PEDRA, jokenpo.TESOURA));
+    assert.equal(jokenpo.PEDRA, jokenpo.jogar(jokenpo.TESOURA, jokenpo.PEDRA));
   });
 
   describe('tesoura empata com tesoura e ganha de papel', function () {
-    assert.equal(labels.EMPATE, jokenpo.jogar(labels.TESOURA, labels.TESOURA));
-    assert.equal(labels.TESOURA, jokenpo.jogar(labels.TESOURA, labels.PAPEL));
-    assert.equal(labels.TESOURA, jokenpo.jogar(labels.PAPEL, labels.TESOURA));
+    assert.equal(jokenpo.EMPATE, jokenpo.jogar(jokenpo.TESOURA, jokenpo.TESOURA));
+    assert.equal(jokenpo.TESOURA, jokenpo.jogar(jokenpo.TESOURA, jokenpo.PAPEL));
+    assert.equal(jokenpo.TESOURA, jokenpo.jogar(jokenpo.PAPEL, jokenpo.TESOURA));
   });
 
   describe('papel empata com papel e ganha de pedra', function () {
-    assert.equal(labels.EMPATE, jokenpo.jogar(labels.PAPEL, labels.PAPEL));
-    assert.equal(labels.PAPEL, jokenpo.jogar(labels.PAPEL, labels.PEDRA));
-    assert.equal(labels.PAPEL, jokenpo.jogar(labels.PEDRA, labels.PAPEL));
+    assert.equal(jokenpo.EMPATE, jokenpo.jogar(jokenpo.PAPEL, jokenpo.PAPEL));
+    assert.equal(jokenpo.PAPEL, jokenpo.jogar(jokenpo.PAPEL, jokenpo.PEDRA));
+    assert.equal(jokenpo.PAPEL, jokenpo.jogar(jokenpo.PEDRA, jokenpo.PAPEL));
   });
 });
